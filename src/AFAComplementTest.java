f05e696df3ec9f8122a6b65002929735f2d7e91e
Islam <islamhamada222@gmail.com>
2020-12-21 06:01:00 +0100

    Test file

--- src/AFAComplementTest.java
+++ src/AFAComplementTest.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class AFAComplementTest {
+import java.util.*;
+
+public class AFAComplementTest {
+    public static void main(String[] args) {
+        HashSet<Set<Character>> init_states = new HashSet<>();
+        init_states.add(new HashSet<>(Arrays.asList('q')));
+
+        HashSet<Character> alphabet = new HashSet<>(Arrays.asList('a','b'));
+        HashSet<Character> acc_states = new HashSet<>(Arrays.asList('q', 's'));
+        Map<Character, Map<Character, Set<Set<Character>>>> trans = new HashMap<>();
+
+        Map<Character, Set<Set<Character>>> q = new HashMap<>();
+        HashSet<Character> d1 = new HashSet<Character>(Arrays.asList('q'));
+        HashSet<Character> d2 = new HashSet<Character>(Arrays.asList('s'));
+        q.put('a', new HashSet<Set<Character>>(Arrays.asList(d1,d2)));
+        trans.put('q', q);
+
+        Map<Character, Set<Set<Character>>> s = new HashMap<>();
+        HashSet<Character> d3 = new HashSet<>(Arrays.asList('q', 's'));
+        s.put('b', new HashSet<Set<Character>>(Arrays.asList(d3)));
+        trans.put('s', s);
+
+        AFA x = new AFA(init_states, alphabet, acc_states, trans);
+        System.out.println(x);
+        System.out.println(x.complement());
+    }
 }
